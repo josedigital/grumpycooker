8be0bdd52c57a02feb146bebff56f548d0b49fdc
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-02-27 21:42:46 -0600

    added text to gridview icon

--- site/templates/markup/posts.php
+++ site/templates/markup/posts.php
@@ -37,7 +37,7 @@ if($small) {
 	if($total) echo "
 		<div id='recipes' class='small-12 columns'>
 			<h3 class='left'>" . sprintf(__('Recipes %1$d to %2$d of %3$d'), $start, $end, $total) . "{$modifier}</h3>
-			<span class='right m-t-1 show-med hide-small'><a href='#' class='make-grid'><i class='".$session->gridview."'></i></a></span>
+			<div class='right m-t-1 show-med hide-small text-center'><a href='#' class='make-grid'><i class='".$session->gridview."'></i><span class='block'>Grid View</span></a></div>
 		</div>";
 }
 
